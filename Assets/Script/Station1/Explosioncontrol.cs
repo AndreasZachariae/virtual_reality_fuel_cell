9fb719e610cde2f1ed6da3c24413ee215a2228fa
FrederikPlahl <frederik@plahl.eu>
2022-04-30 23:10:05 +0200

    Fix bugs

--- Assets/Script/Station1/Explosioncontrol.cs
+++ Assets/Script/Station1/Explosioncontrol.cs
@@ -8,20 +8,22 @@ public class Explosioncontrol : MonoBehaviour
     public GameObject Fuel_Cell;
     private Animator anim;
     private Slider ExplosionSlider;
+
     // Start is called before the first frame update
     void Start()
     {
-        ExplosionSlider = GetComponent<ExplosionSlider>();
+        ExplosionSlider = GetComponent<Slider>();
     }
+
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     public void applyExplosion()
     {
         anim = Fuel_Cell.GetComponent<Animator>();
-        if (anim)
+        if(anim)
         {
             anim.SetFloat("ExplosionSlider", ExplosionSlider.value);
         }
