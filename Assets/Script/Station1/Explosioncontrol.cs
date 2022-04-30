c39d7ea52f5c4c16349dd1b038046156eb84fc24
FrederikPlahl <frederik@plahl.eu>
2022-04-30 22:55:52 +0200

    Add exploded view animation

---
+++ Assets/Script/Station1/Explosioncontrol.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Explosioncontrol : MonoBehaviour
+{
+    public GameObject Fuel_Cell;
+    private Animator anim;
+    private Slider ExplosionSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ExplosionSlider = GetComponent<ExplosionSlider>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void applyExplosion()
+    {
+        anim = Fuel_Cell.GetComponent<Animator>();
+        if (anim)
+        {
+            anim.SetFloat("ExplosionSlider", ExplosionSlider.value);
+        }
+    }
+}
